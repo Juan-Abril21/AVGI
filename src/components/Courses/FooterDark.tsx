816c52c445c4b9a4792452f5f02e1ed743fe2e27
Juan Abril <jdac149@gmail.com>
2024-12-18 15:37:06 -0500

    Fix footer

--- src/components/Courses/FooterDark.tsx
+++ src/components/Courses/FooterDark.tsx
@@ -5,110 +5,116 @@ import { Link } from "react-router-dom";
 const FooterDark = () => {
   return (
     <>
-    <div className="linea-footer-navbarDark"></div>
-    <footer className="footer">
-      <div className="footer-container">
-        <div className="logo-section">
-          <div className="logo-container">
-            <img src="/Big Logo white.png" alt="AVGI Group Logo" />
-          </div>
-          <p className="company-descriptionDark">
-            Think up solutions in your own way.
-          </p>
-          <div className="social-iconsDark">
-            <a href="https://www.instagram.com/" target="_blank">
-              <LuInstagram />
-            </a>
-            <a href="https://www.facebook.com/" target="_blank">
-              <LuFacebook />
-            </a>
-            <a href="https://www.linkedin.com/" target="_blank">
-              <LuLinkedin />
-            </a>
-            <a href="mailto:Global@avgi.group">
-              <LuMail />
-            </a>
-          </div>
-        </div>
-
-        <div className="footer-section">
-          <h3 className="section-title-aboutDark">About</h3>
-          <div className="contact-info">
-            <p className="contact-itemDark">
-              <span className="contact-label">Direction - </span>
-              P.O. BOX 2516, Elizabeth, NJ 07207
+      <div className="linea-footer-navbarDark"></div>
+      <footer className="footerDark">
+        <div className="footer-container">
+          <div className="logo-section">
+            <div className="logo-container">
+              <img src="/Big Logo white.png" alt="AVGI Group Logo" />
+            </div>
+            <p className="company-descriptionDark">
+              Think up solutions in your own way.
             </p>
-            <a
-              className="contact-itemDark"
-              href="mailto:Global@avgi.group"
-              style={{ cursor: "pointer" }}
-            >
-              <a className="contact-label">Email - </a>
-              Global@avgi.group
-            </a>
-            <a className="contact-itemDark" href="tel:9298378939">
-              <span className="contact-label">Phone - </span>
-              929 - 837 - 8929
-            </a>
+            <div className="social-iconsDark">
+              <a href="https://www.instagram.com/" target="_blank">
+                <LuInstagram />
+              </a>
+              <a href="https://www.facebook.com/" target="_blank">
+                <LuFacebook />
+              </a>
+              <a href="https://www.linkedin.com/" target="_blank">
+                <LuLinkedin />
+              </a>
+              <a href="mailto:Global@avgi.group">
+                <LuMail />
+              </a>
+            </div>
           </div>
-        </div>
 
-        <div className="footer-section">
-          <h3 className="section-titleDark">Explore</h3>
-          <div className="nav-links">
-            <Link to="/" className="nav-link-footerDark">
-              Home
-            </Link>
-            <Link to="/whoweare" className="nav-link-footerDark">
-              Who we are
-            </Link>
-            <Link to="/pricing" className="nav-link-footerDark">
-              Pricing
-            </Link>
-            <Link to="/contact" className="nav-link-footerDark">
-              Contact
-            </Link>
-            <Link to="/login" className="nav-link-footerDark">
-              Courses
-            </Link>
+          <div className="footer-section">
+            <h3 className="section-title-aboutDark">About</h3>
+            <div className="contact-info">
+              <p className="contact-itemDark">
+                <span className="contact-label">Direction - </span>
+                P.O. BOX 2516, Elizabeth, NJ 07207
+              </p>
+              <a
+                className="contact-itemDark"
+                href="mailto:Global@avgi.group"
+                style={{ cursor: "pointer" }}
+              >
+                <a className="contact-label">Email - </a>
+                Global@avgi.group
+              </a>
+              <a className="contact-itemDark" href="tel:9298378939">
+                <span className="contact-label">Phone - </span>
+                929 - 837 - 8929
+              </a>
+            </div>
           </div>
-        </div>
 
-        <div className="footer-section">
-          <h3 className="section-titleDark">Socials</h3>
-          <div className="nav-links">
-            <a
-              href="https://www.facebook.com/"
-              target="_blank"
-              className="nav-link-footerDark"
-            >
-              Facebook
-            </a>
-            <a
-              href="https://www.instagram.com/"
-              target="_blank"
-              className="nav-link-footerDark"
-            >
-              Instagram
-            </a>
-            <a
-              href="https://www.linkedin.com/"
-              target="_blank"
-              className="nav-link-footerDark"
-            >
-              LinkedIn
-            </a>
-            <a href="https://www.x.com/" target="_blank" className="nav-link-footerDark">
-              Twitter - X
-            </a>
+          <div className="footer-section">
+            <h3 className="section-titleDark">Explore</h3>
+            <div className="nav-links">
+              <Link to="/" className="nav-link-footerDark">
+                Home
+              </Link>
+              <Link to="/whoweare" className="nav-link-footerDark">
+                Who we are
+              </Link>
+              <Link to="/pricing" className="nav-link-footerDark">
+                Pricing
+              </Link>
+              <Link to="/contact" className="nav-link-footerDark">
+                Contact
+              </Link>
+              <Link to="/login" className="nav-link-footerDark">
+                Courses
+              </Link>
+            </div>
+          </div>
+
+          <div className="footer-section">
+            <h3 className="section-titleDark">Socials</h3>
+            <div className="nav-links">
+              <a
+                href="https://www.facebook.com/"
+                target="_blank"
+                className="nav-link-footerDark"
+              >
+                Facebook
+              </a>
+              <a
+                href="https://www.instagram.com/"
+                target="_blank"
+                className="nav-link-footerDark"
+              >
+                Instagram
+              </a>
+              <a
+                href="https://www.linkedin.com/"
+                target="_blank"
+                className="nav-link-footerDark"
+              >
+                LinkedIn
+              </a>
+              <a
+                href="https://www.x.com/"
+                target="_blank"
+                className="nav-link-footerDark"
+              >
+                Twitter - X
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-    </footer>
-    <div className="lineaFooterDark"></div>
-    <div className="terms-container">
-        <div className="terms-grid">
-          <div className="rightsDark">©2024 AVGI Group. All rights reserved</div>
+      </footer>
+      <div className="lineaFooterDark"></div>
+      <div className="terms-containerDark">
+        <div className="terms-gridDark">
+          <div className="rightsDark">
+            ©2024 AVGI Group. All rights reserved
+          </div>
           <div className="rights-container">
             <div className="termsDark">
               <Link to="/">Terms & Conditions</Link>
@@ -116,12 +122,15 @@ const FooterDark = () => {
             <div className="privacyDark">
               <Link to="/">Privacy & Policy</Link>
             </div>
-            <a
-              className="developedDark"
-              href="https://webelopers.io/#home"
-              target="_blank"
-            >
-              Developed by <a className="gradient-developedDark">WEBELOPERS</a>
+            <a className="developedDark">
+              Developed by{" "}
+              <a
+                className="gradient-developedDark"
+                href="https://webelopers.io/#home"
+                target="_blank"
+              >
+                WEBELOPERS
+              </a>
             </a>
           </div>
         </div>
